37024c375b540bd7141cd2978bea554c849f4769
JW Nicholson <john.nicholson812@gmail.com>
2020-07-28 14:50:53 -0400

    starting to layout logic psuedo code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-
 import './App.css';
+
+import Grid from './components/Grid';
 
 //import Grid from './components/Grid';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Conway's Game Of Life</h1>
-
-      </div>
+    
+<div className="App">
+<header className="App-header">
+  <h1>Conways Game of Life</h1>
+  <p>
+    My explanation
+  </p>
+  </header>
+ <div className="gameCtnr">
+   Control Buttons
+ </div>
+     <ul>
+       <li>Grid</li>
+       <li>Rules</li>
+       <li>content</li>
+       <li>footer</li>
+     </ul>
+ 
+ 
+ 
+</div>
     )
   }
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import Cell from './components/Cell'
+//import Cell from './components/Cell'
 
 export default class Grid extends Component {
     render() {
         return (
             <div>
-                <Cell />
+                <ul>
+                    <li>Cell</li>
+                    <li>Build Grid</li>
+                    <li>render grid</li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         )
     }
