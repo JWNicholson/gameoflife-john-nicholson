9d855e6a8a483aea201967ee4aeac8b59f97d95a
JW Nicholson <john.nicholson812@gmail.com>
2020-07-28 13:47:48 -0400

    basic framework layed out, including meyers reset css

---
+++ src/components/Grid.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Cell from './components/Cell'
+
+export default class Grid extends Component {
+    render() {
+        return (
+            <div>
+                <Cell />
+            </div>
+        )
+    }
+}
