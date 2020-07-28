dc41b781fadda8379ade87582f3aef9e64eb178b
JW Nicholson <john.nicholson812@gmail.com>
2020-07-28 15:30:15 -0400

    Grid and Cell components and functions done for testing

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react'
 //import Cell from './components/Cell'
 
 export default class Grid extends Component {
-    renderCell(){
+    renderCell = i =>{
         //set styling (dead/alive)
+        return (
+            <Cell 
+            key={i} 
+            cellstyle={this.props.cellstyle(i)} 
+            onClick={() => this.props.onClick(i)} />
+        )
     }
 
     renderGrid(){
         let grid = []
         //for loop to build 25 x 25 grid
+        for(let i =0;i < 25; i++){
+            let childrens = [];
+            for(let x=0; x < 25; x++){
+                childrens.push(this.renderCell(x + i * 25))
+            }
+            grid.push(<div key={i} className="grid-row">{childrens}</div>);
+        }
+        return grid
     }
 
     render() {
         return (
             <div>
-               rendered Grid
+               {this.renderGrid()}
             </div>
         )
     }
